37d6efc55efd7f675ba3c4da6dd6488b4bd00397
Momtaz1992 <momtaz.mostafa.asmar@gmail.com>
2023-08-11 23:18:25 +0200

    media  with menu  shop is done

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 // navbar
 let navbar = document.querySelector(".navbar");
 
-document.querySelector("#menu-btn").onclick=() => {
+document.querySelector("#menu-btn").onclick=() =>{
        navbar.classList.toggle("active");
 }
+let cartItem = document.querySelector(".cart-items-container");
+
+document.querySelector(".bx-cart-alt").onclick=()=>{
+       cartItem.classList.toggle("active");
+}
+let searchForm = document.querySelector(".search-form");
+
+document.querySelector("#search-btn").onclick=()=>{
+       searchForm.classList.toggle("active");
+}
+const video = document.querySelector("video");
+
+video.addEventListener("play", () => {
+  video.muted = true;
+});
+
+video.addEventListener("ended", () => {
+  video.play();
+});
